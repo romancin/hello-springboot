d5f9c439f280d5bba133bbbce709b9eda5ec86b9
injninja <al.rossi87@gmail.com>
2021-05-25 22:46:16 +0200

    Replaced with springbootstarter

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.6.0-jdk-11-slim AS build
-COPY src /app/src
-COPY pom.xml /app
-RUN mvn -f /app/pom.xml clean package
-
-FROM openjdk:11-jre-slim
-COPY --from=build //app/target/*.jar /usr/local/lib/demo.jar
-EXPOSE 8080
-ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
--- src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -1,10 +1,10 @@
-package it.kubealex.helloworld;
+package com.example.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloWorldApplicationTests {
+class HelloworldApplicationTests {
 
 	@Test
 	void contextLoads() {
